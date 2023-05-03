fc4777093a5bc4b9b411956a314f0cee85919ed3
unknown <anikm2982@gmail.com>
2023-05-04 00:34:58 +0600

    vercel.js added

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
-const app = express();
 const port = 5000;
+const cors = require("cors");
 
-// Define a route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const app = express();
+app.use(cors());
+
+const recipes = require('./Data/data.json')
+const traditionalFood = require('./Data/dishes.json')
+
+app.get('/', (req, res) =>{
+  res.send(recipes)
+})
+
+app.get('/recipe/:id', (req, res) =>{
+  const id = req.params.id;
+  const singleRecipe = recipes.find(rs => rs.id == id)
+  res.send(singleRecipe)
+})
+
+app.get('/traditionalFood', (req, res) =>{
+  res.send(traditionalFood);
+})
 
 // Start the server
 app.listen(port, () => {
